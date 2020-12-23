fbed5c73d2552859eacbeb855f163e9b05d2086e
Sam Buckberry <SamBuckberry@users.noreply.github.com>
2020-12-23 14:59:00 +0800

    Update call_dmr_dmrseq_dug.R

--- R_scripts/call_dmr_dmrseq_dug.R
+++ R_scripts/call_dmr_dmrseq_dug.R
@@ -20,14 +20,12 @@ library(batchtools)
 tmpl <- system.file(package="batchtools", "templates", "slurm-simple.tmpl")
 noquote(readLines(tmpl))
 
-
-
+tmpl <- "/d/home/hg19ips/hg19ips_samb/mcc_hg19ips/workflow/R_scripts/slurm-dmr-template.tmpl"
 
 FUN <- function(i) system("hostname", intern=TRUE)
-library(BiocParallel)
 
 ## register SLURM cluster instructions from the template file
-param <- BatchtoolsParam(workers=5, cluster="slurm", template=tmpl)
+param <- BatchtoolsParam(workers=50, cluster="slurm", template=tmpl)
 register(param)
 ## do work
 xx <- bplapply(1:100, FUN)
@@ -66,15 +64,41 @@ pData(obj_list)$CellType <- factor(c(rep("ESC", times=5), rep("Naive2Primed", ti
 pData(obj_list)$Replicate <- c(1:5,1:5)
 
 
+md <- read.csv(file = "/d/home/hg19ips/hg19ips_samb/mcc_hg19ips/workflow/resources/sample_metadata.csv")
+colnames(md)
+
+obj_list <- readRDS("/d/home/hg19ips/hg19ips_samb/mcc_hg19ips/data/processed_data/bsseq_cg/all_samples_BSseq_obj.rds")
+
+library(stringr)
+
+# Ensure metadata and data are in same order
+stopifnot(all(str_sub(md$cg_bsseq, start = 1, end = 6) == str_sub(colnames(obj_list), start = 1, end = 6)))
+
+colnames(obj_list) <- md$library
 
 
-obj_list <- readRDS("/home/sbuckberry/working_data_02/polo_project/human_ips/methylCseq/processed_data/dmrseq_dmrs/esc_vs_n2p_BSseq_obj.rds")
+# Subset to samples for testing
+keep_ids <- c("Primed", "TNT")
+obj_list <- obj_list[ ,md$group %in% keep_ids]
 
-primed_n2p_dmrs <- dmrseq(obj_list, testCovariate = "CellType",
+
+md <- md[md$group %in% keep_ids, ]
+stopifnot(all(colnames(obj_list) == md$library))
+
+
+
+# Remove CpG with no coverage
+loci.idx <- which(DelayedMatrixStats::rowSums2(getCoverage(obj_list, type="Cov")==0) == 0)
+obj_list <- obj_list[loci.idx, ]
+
+# Setup the groups
+pData(obj_list)$CellType <- md$group
+
+primed_tnt_dmrs <- dmrseq(obj_list, testCovariate = "CellType",
                           bpSpan = 500,
                           maxGap = 500,
                           maxPerms = 10)
 
-saveRDS(object = primed_n2p_dmrs,
+saveRDS(object = primed_tnt_dmrs,
         file = "/home/sbuckberry/working_data_02/polo_project/human_ips/methylCseq/processed_data/dmrseq_dmrs/esc_vs_n2p_dmrseq_DMRs.rds")
 
