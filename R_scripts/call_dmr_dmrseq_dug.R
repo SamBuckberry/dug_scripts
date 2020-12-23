adcdc1ee982975081649f80c69a0462d8d27070e
Sam Buckberry <SamBuckberry@users.noreply.github.com>
2020-12-23 12:10:18 +0800

    Update call_dmr_dmrseq_dug.R

--- R_scripts/call_dmr_dmrseq_dug.R
+++ R_scripts/call_dmr_dmrseq_dug.R
@@ -13,11 +13,36 @@ library(data.table)
 # More in depth documentation at https://bioconductor.org/packages/release/bioc/manuals/BiocParallel/man/BiocParallel.pdf
 # and here https://hpc.nih.gov/apps/R.html#biocparallel
 
+library(BiocParallel)
+library(Rmpi)
+library(batchtools)
+
+tmpl <- system.file(package="batchtools", "templates", "slurm-simple.tmpl")
+noquote(readLines(tmpl))
+
+
+
+
+FUN <- function(i) system("hostname", intern=TRUE)
+library(BiocParallel)
+
+## register SLURM cluster instructions from the template file
+param <- BatchtoolsParam(workers=5, cluster="slurm", template=tmpl)
+register(param)
+## do work
+xx <- bplapply(1:100, FUN)
+table(unlist(xx))
+
+
+
+
 BiocParallel::register(BPPARAM = BatchtoolsParam(workers=100, cluster="slurm"))
 
 FUN <- function(i) system("hostname", intern=TRUE)
 
 xx <- bplapply(1:100, FUN)
+
+
 
 library(BiocParallel)
 library(Rmpi)
