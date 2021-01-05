c42da30c2be9d5071c979660f1f36fa4c4f74e0e
Sam Buckberry <SamBuckberry@users.noreply.github.com>
2021-01-05 13:33:39 +0800

    Update call_dmr_dmrseq_dug.R

--- R_scripts/call_dmr_dmrseq_dug.R
+++ R_scripts/call_dmr_dmrseq_dug.R
@@ -4,18 +4,37 @@ library(magrittr)
 library(stringr)
 library(BiocParallel)
 library(data.table)
+library(Rmpi)
+library(batchtools)
+
+args = commandArgs(TRUE)
+
+#  
+manifest <- args[1]
+out_path <- args[2]
+prefix <- args[3]
+
+#-------- Read the DMR manifest file
+
+# Check that the manifest file exists
+(!file.exists(manifest)){
+    cat('Cannot find', manifest, 'exiting!\n')
+    stop()
+}
+
+# Read the manifest file
+man_dat <- read.table(manifest, header = TRUE, sep = ",")
+
+# Check that files exist
 
 
-#--------Call DMRs
 
-# This is where we need to the parallel processing. 
+#-------- Call DMRs
+
+# This is where we need to the parallel processing.
 # See page 12 of https://www.bioconductor.org/packages/release/bioc/vignettes/BiocParallel/inst/doc/Introduction_To_BiocParallel.pdf
 # More in depth documentation at https://bioconductor.org/packages/release/bioc/manuals/BiocParallel/man/BiocParallel.pdf
 # and here https://hpc.nih.gov/apps/R.html#biocparallel
-
-library(BiocParallel)
-library(Rmpi)
-library(batchtools)
 
 tmpl <- system.file(package="batchtools", "templates", "slurm-simple.tmpl")
 noquote(readLines(tmpl))
@@ -25,34 +44,39 @@ tmpl <- "/d/home/hg19ips/hg19ips_samb/mcc_hg19ips/workflow/R_scripts/slurm-dmr-t
 FUN <- function(i) system("hostname", intern=TRUE)
 
 ## register SLURM cluster instructions from the template file
-param <- BatchtoolsParam(workers=50, cluster="slurm", template=tmpl)
+## Workers is set to 22, as one worker per chromosome (autosomes only) is required
+param <- BatchtoolsParam(workers=22, cluster="slurm", template=tmpl)
 register(param)
 ## do work
 xx <- bplapply(1:100, FUN)
 table(unlist(xx))
 
 
-
-
-BiocParallel::register(BPPARAM = BatchtoolsParam(workers=100, cluster="slurm"))
+BiocParallel::register(BPPARAM = BatchtoolsParam(workers=22, cluster="slurm"))
 
 FUN <- function(i) system("hostname", intern=TRUE)
 
 xx <- bplapply(1:100, FUN)
 
-
-
-library(BiocParallel)
-library(Rmpi)
 FUN <- function(i) system("hostname", intern=TRUE)
 
 param <- SnowParam(mpi.universe.size() - 1, "MPI")
 register(param)
 
+# Read a Bs_seq boject from .Rds file
+read_bs_obj <- function(rds_path, chroms){
 
+   bs_obj <- readRDS(file = rds_path)
+   bs_obj <- strandCollapse(bs_obj)
+   bs_obj <- chrSelectBSseq(BSseq = bs_obj,
+                            seqnames = chroms,
+                            order = TRUE)
+   return(bs_obj)
 
-# Load the data
-obj_list <- lapply(X = obj_fls, read_bs_obj)
+}
+
+# Load the data, and sub-select for autosomes
+obj_list <- lapply(X = obj_fls, read_bs_obj, chroms = str_c("chr", 1:22))
 obj_list <- bsseq::combineList(x = obj_list)
 
 # Remove CpG with no coverage
@@ -69,8 +93,6 @@ colnames(md)
 
 obj_list <- readRDS("/d/home/hg19ips/hg19ips_samb/mcc_hg19ips/data/processed_data/bsseq_cg/all_samples_BSseq_obj.rds")
 
-library(stringr)
-
 # Ensure metadata and data are in same order
 stopifnot(all(str_sub(md$cg_bsseq, start = 1, end = 6) == str_sub(colnames(obj_list), start = 1, end = 6)))
 
@@ -85,8 +107,6 @@ obj_list <- obj_list[ ,md$group %in% keep_ids]
 md <- md[md$group %in% keep_ids, ]
 stopifnot(all(colnames(obj_list) == md$library))
 
-
-
 # Remove CpG with no coverage
 loci.idx <- which(DelayedMatrixStats::rowSums2(getCoverage(obj_list, type="Cov")==0) == 0)
 obj_list <- obj_list[loci.idx, ]
@@ -94,11 +114,7 @@ obj_list <- obj_list[loci.idx, ]
 # Setup the groups
 pData(obj_list)$CellType <- md$group
 
-primed_tnt_dmrs <- dmrseq(obj_list, testCovariate = "CellType",
-                          bpSpan = 500,
-                          maxGap = 500,
-                          maxPerms = 10)
+primed_tnt_dmrs <- dmrseq(obj_list, testCovariate = "CellType", cutoff=0.05)
 
 saveRDS(object = primed_tnt_dmrs,
         file = "/home/sbuckberry/working_data_02/polo_project/human_ips/methylCseq/processed_data/dmrseq_dmrs/esc_vs_n2p_dmrseq_DMRs.rds")
-
