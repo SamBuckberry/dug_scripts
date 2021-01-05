40d5a1c3dbea930b751f7f01e17b12eca52b1c20
Sam Buckberry <SamBuckberry@users.noreply.github.com>
2021-01-05 15:07:42 +0800

    Update submit_dmr.job.sh

--- call_dmr/submit_dmr.job.sh
+++ call_dmr/submit_dmr.job.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+#SBATCH --job-name=submit_dmr.job
+#SBATCH --output=submit_dmr.out
+#SBATCH --error=submit_dmr.err
+
+# Add the modules needed for the R script
 module add R
+
+Rscript ./run_dmrseq_dug.R \
+/d/home/hg19ips/hg19ips_samb/mcc_hg19ips/workflow/call_dmr/dmr_manifest_test.csv \
+/d/home/hg19ips/hg19ips_samb/mcc_hg19ips/data/dmrs/ \
+test_data
+
+
+
